7ac7df342148a619c2d5a3f8a1340a130de64396
Harshit-Shrivastava <harshit.shrivastava14@gmail.com>
2016-11-23 19:09:40 -0500

    Initial attempt at creating the decision tree

--- part1/tree.py
+++ part1/tree.py
@@ -1,12 +1,6 @@
+from node import node
+
 class decisionTree:
-    def __init__(self, attribute, factor, dataTable):
-        self.attribute = attribute  #the word on which splitting happens
-        self.factor = factor    #split factor
-        self.left = None    #left subtree
-        self.right = None   #right subtree
-        self.parent = None  #parent of this node
-        self.result = ''    #result at this node
-        self.dataTable = dataTable  #datatable at this node
 
     def hasLeft(self):
         return self.left
