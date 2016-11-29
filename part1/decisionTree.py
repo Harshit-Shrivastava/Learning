f465d54e83e264a2fb3e863c1819f6fa8654c3b7
Harshit-Shrivastava <harshit.shrivastava14@gmail.com>
2016-11-29 17:20:47 -0500

    Decision tree based on continuous frequency distributions

--- part1/decisionTree.py
+++ part1/decisionTree.py
@@ -1,105 +1,40 @@
-import copy
 import operator
 from math import log
 from node import node
-'''
-def createTree(root, att, tempDataTable):
-	#if root.result!=None:
-	#	return root
-	if len(att) == 0 or root.result == 'spam' or root.result == 'notspam': ##
-		if root.positive > root.negative:
-			root.result = 'spam'
-		else:
-			root.result = 'notspam'
-		return root
-	else:
-		attribute = att[0]
-		factor = getSplitFactor(attribute, tempDataTable)	#smaller values in left, larger in right subtree
-		leftDataTable, rightDataTable = splitTrainingset(attribute, factor, tempDataTable)
-		pos, neg = getPosNeg(tempDataTable)
-		#root = node(attribute,factor,tempDataTable)
-		root.createNode(attribute, factor, tempDataTable)
-		att.remove(attribute)
-		root.positive = pos		#positive for spam
-		root.negative = neg		#negative for not spam
-		if root.positive == 0:
-			root.result = 'notspam'
-		elif root.negative == 0:
-			root.result = 'spam'
-		root.leftNode = createTree(node(), att, leftDataTable)		#left subtree
-		root.rightNode = createTree(node(), att, rightDataTable)	#right subtree
-'''
 
-def createTree(tempDataTable, attributes, root = None):
+overfittingFactor = 40
+thresholdCount = 2
 
-	# we will find two things-
-	# 1. whether all the rows in tempDataTable are homogenous
-	# 2. To prevent overfitting, if size of tempDataTable gets smaller than 50, then
-	# declare the majority class as root.result
+def createTree(tempDataTable, attributes):
 	if(homogenous(tempDataTable)):
 		pos, neg = getPosNeg(tempDataTable)
+		root = node(None,None,None)
 		if pos > neg:
 			root.result = 'spam'
 		else:
 			root.result = 'notspam'
 		return root
 	if (overfitting(tempDataTable)):
+		root = node(None, None, None)
 		root.result = getOverfittingResult(tempDataTable)
 		return root
 	attribute, factor = chooseAttribute(attributes, tempDataTable)
+	#rows having value of the attribute <= factor form the left sub-tree,
+	#rows having value of the attribute > factor form the right of the tree
 	leftDataTable, rightDataTable = splitTrainingset(attribute, attributes, factor, tempDataTable)
-	pos, neg = getPosNeg(tempDataTable)
-	root = node(attribute, factor, tempDataTable)
+	pos, neg = getPosNeg(tempDataTable)		#count of spams and notspams
+	root = node(attribute, factor, tempDataTable)	#create the node with reqd info
 	root.positive = pos  # positive for spam
 	root.negative = neg  # negative for not spam
-	if root.positive == 0:
-		root.result = 'notspam'
-	elif root.negative == 0:
-		root.result = 'spam'
-	root.left = createTree(leftDataTable, attributes, root)
-	root.right = createTree(rightDataTable, attributes, root)
+	#print root.result
+	#print attribute
+	#print leftDataTable
+	#print rightDataTable
+	root.left = createTree(leftDataTable, attributes)		#recursively create left subtree
+	root.right = createTree(rightDataTable, attributes)	#recursively create right subtree
 	return root
 
-def homogenous(tempDataTable):
-	pos, neg = getPosNeg(tempDataTable)
-	if pos ==0 or neg == 0:
-		return True
-	else:
-		return False
-
-def overfitting(tempDataTable):
-	#TODO: change this to 50
-	if len(tempDataTable) < 100:
-		return True
-
-def getOverfittingResult(tempDataTable):
-	pos, neg = getPosNeg(tempDataTable)
-	if pos>neg:
-		return 'spam'
-	else:
-		return 'notspam'
-
 def chooseAttribute(attributes, dataTable):
-	#allFactors = []
-	#igList = []
-	'''
-	for attr in attributes:
-		s,e = getRange(attr, attributes, dataTable)
-		factors = findAllFactors(s, e)
-		igf = []
-		for f in factors:
-			i = informationGain(f, attr, attributes, dataTable)
-			igf.append(i)
-		ind = igf.index(max(igf))
-		igList.append(igf[ind])
-		allFactors.append(factors[ind])
-	igIndex = igList.index(max(igList))
-	#TODO: Correct the mistake here. Take minimum entropy instead of maximum
-	#factIndex = allFactors.index(max(entropies))
-	maxFactor = allFactors[igIndex]
-	splitAttribute = attributes[igIndex]
-	return splitAttribute, maxFactor'''
-
 	bestThresholds = []
 	bestGains = []
 	for j in range(len(dataTable[0])-1):
@@ -114,7 +49,6 @@ def chooseAttribute(attributes, dataTable):
 		max_index, max_value = max(enumerate(gains), key=operator.itemgetter(1))
 		bestGains.append(max_value)
 		bestThresholds.append(thresholds[max_index])
-
 	max_index, max_value = max(enumerate(bestGains), key=operator.itemgetter(1))
 	finalThreshold = bestThresholds[max_index]
 	return attributes[max_index],finalThreshold
@@ -127,17 +61,16 @@ def getRange(attr, attributes, dataTable):
 	return low, high
 
 def findAllFactors(low, high):
-	
 	if (low>high):
 		raise Exception('Low can not be higher than high ')
 	if low == high:
 		factors = []
 		factors.append(low)
 		return factors
-	val = (high-low)/4
+	val = (high-low)/thresholdCount
 	factors = []
 	item = low
-	for i in range(1, 4):
+	for i in range(1, thresholdCount):
 		item += val
 		factors.append(item)
 	return factors
@@ -158,39 +91,16 @@ def findInformationGain(threshold, wordIndex, attributes, dataTable):
 			less.append(dataTable[i])
 		else:
 			more.append(dataTable[i])
-
 	e,e1,e2 = getEntropy(dataTable),getEntropy(less),getEntropy(more)
 	w1 = float(len(less))/len(dataTable)
 	w2 = float(len(more))/len(dataTable)
 	gain = e - (e1*w1 + e2*w2)
 	return gain
 
-def informationGain(factor, attr, attributes, dataTable):
-	index = attributes.index(attr)
-	newlist = [item[index] for item in dataTable]
-	less = float(len([1 for i in newlist if i < factor]))
-	more = float(len([1 for i in newlist if i >= factor]))
-	if (less+more) == 0:
-		return 0
-	else:
-		pless = float(less/(less+more))
-		pmore = float(more/(less+more))
-		entropy = (-1)*(pless*log(pless if pless > 0 else 1)) - (pmore*log(pmore if pmore>0 else 1))
-		#TODO: Ask how to calculate information gain using entropy over all the attributes
-		return entropy
-
-#gets the index of the attribute in the table. For now it is always the first attribute,
-#but later, the attribute will be decided based on entropy
-def getIndex(attribute, table):
-	for i, j in enumerate(table):
-		if attribute in j:
-			return (i, j.index(attribute))
-
 #calculates the positive and negative count in order to calculate the confidence factor
 def getPosNeg(tempDataTable):
 	pos = 0
 	neg = 0
-	#classArray = transpose(tempDataTable)[0]
 	for i in range (0,len(tempDataTable)):
 		if tempDataTable[i][len(tempDataTable[0]) - 1] == 1:
 			pos += 1
@@ -201,25 +111,44 @@ def getPosNeg(tempDataTable):
 #this function creates two subtables with values for the attribute less than and greater than
 #the splitting factor
 def splitTrainingset(attribute, attributes, value, tempDataTable):
-	leftDataTable = copy.deepcopy(tempDataTable)
-	rightDataTable = copy.deepcopy(tempDataTable)
+	leftDataTable = []
+	rightDataTable = []
 	index = attributes.index(attribute)
 	for i in range (0, len(tempDataTable)):
-		if tempDataTable[i][index] > value:
-			leftDataTable.remove(tempDataTable[i])
+		if tempDataTable[i][index] <= value:
+			leftDataTable.append(tempDataTable[i])
 		else:
-			rightDataTable.remove(tempDataTable[i])
+			rightDataTable.append(tempDataTable[i])
 	return leftDataTable, rightDataTable
+
+def homogenous(tempDataTable):
+	pos, neg = getPosNeg(tempDataTable)
+	if pos ==0 or neg == 0:
+		return True
+	else:
+		return False
+
+def overfitting(tempDataTable):
+	if len(tempDataTable) < overfittingFactor:
+		return True
+
+def getOverfittingResult(tempDataTable):
+	pos, neg = getPosNeg(tempDataTable)
+	if pos>neg:
+		return 'spam'
+	else:
+		return 'notspam'
 
 #traversing the data tree to test the document as spam or notspam
 def traverseTree(root, dataRow, attributes):
 	temp = root
-
 	while temp.result == None:
 		tempAttr = temp.attribute
 		tempFactor = temp.factor
-		attrIndex = attributes.index(tempAttr)
-		dataRowValue = dataRow[attrIndex]
+		dataRowValue = 0
+		if tempAttr in attributes:
+			attrIndex = attributes.index(tempAttr)
+			dataRowValue = dataRow[attrIndex]
 		if dataRowValue <= tempFactor:
 			temp = temp.left
 		else:
--- part1/spam.py
+++ part1/spam.py
@@ -7,9 +7,10 @@ import copy
 import numpy as np
 import pickle
 from decisionTree import createTree, traverseTree
+from binaryDecisionTree import decisionTree
 from node import node
 epsilon = 0.0000000001
-dividend = 500
+dividend = 250
 
 def trainNaiveBayes(data_directory,model):
 	#print 'Hello-word!,'.translate(None, string.punctuation)
@@ -162,6 +163,9 @@ def trainDecisionTree(data_directory,model):
 
 	union_set = set(b1).union(set(b2))
 	attributes = [w[0] for w in union_set]
+	if '' in attributes:
+		attributes.remove('')
+
 	attributeSet = set(attributes)
 
 	dataTable = []
@@ -209,16 +213,16 @@ def trainDecisionTree(data_directory,model):
 
 	# each attributes is a word which is stored on the list called 'attributes'
 	# so, dataTable has 2646 rows and each row has 7417 attributes where the last attribute is target attribute
-	print len(dataTable)
-	print len(dataTable[0])
-	print attributes
+	#print len(dataTable)
+	#print len(dataTable[0])
+	#print attributes
 	#print attributeSet
-	print dataTable[0]
+	#print dataTable[0]
 	# build the decision tree
 	# write the decision tree model on the filename passed as 'model' so that later we can load the model and test new data instances
 	#creating the decision tree
 	# recursively create the decision tree
-	root = createTree(dataTable, attributes, root = None)
+	root = createTree(dataTable, attributes)
 	print root
 	#save the decision tree into the model file
 	with open(model, 'wb') as output:
@@ -263,6 +267,9 @@ def testDecisionTree(data_directory,model):
 
 	union_set = set(b1).union(set(b2))
 	attributes = [w[0] for w in union_set]
+	if '' in attributes:
+		attributes.remove('')
+
 	attributeSet = set(attributes)
 
 	dataTable = []
@@ -310,6 +317,230 @@ def testDecisionTree(data_directory,model):
 
 	# each attributes is a word which is stored on the list called 'attributes'
 	# so, dataTable has 2646 rows and each row has 7417 attributes where the last attribute is target attribute
+	#print len(dataTable)
+	#print len(dataTable[0])
+	#print attributes
+	# print attributeSet
+	#print dataTable[0]
+	print 'Reading decision tree from memory'
+	root = pickle.load(open(model, 'rb'))
+	#print (root)
+	print 'Claasifying the emails'
+	correct = 0
+	spam = 0
+	notspam = 0
+	for i in range (0, len(dataTable)):
+		dataTableRow = dataTable[i]
+		#print('%d. Acutal label = %s and Predicted Label = ')
+		if dataTableRow[len(dataTableRow) -1] == 1:
+			actualResult = 'spam'
+		else:
+			actualResult = 'notspam'
+		predictedResult = traverseTree(root, dataTableRow, attributes)
+		if predictedResult == 'spam':
+			spam += 1
+		else:
+			notspam += 1
+		if actualResult == predictedResult:
+			#print('%d. Acutal label = %s and Predicted Label = %s and its a success'%(i,actualResult,predictedResult))
+			correct += 1
+		#else:
+			#print('%d. Acutal label = %s and Predicted Label = %s and its a failure' % (i, actualResult, predictedResult))
+	print 'Accuracy %f' %(float(correct)/len(dataTable))
+	print 'Correctly classified %d emails out of %d' % (correct, len(dataTable))
+	print 'Clasified %d emails as spam and %d emails as not-spam' % (spam, notspam)
+
+def trainBinaryDecisionTree(data_directory, model):
+	nonspamDict = {}
+	spamDict = {}
+	print('Training the Decision Tree Classifier... Please wait')
+	for fname in os.listdir(data_directory + '/spam'):
+		with open(data_directory + '/spam/' + fname, 'r') as f:
+			lines = f.readlines()
+		for line in lines:
+			for w in line.strip().split(' '):
+				word = w.lower()
+				# if word == '':
+				#	raw_input()
+				if word not in ('', ' ', 'the', 'to'):
+					word = word.translate(None, string.punctuation)
+					if spamDict.get(word, None) == None:
+						spamDict[word] = 1.0
+					else:
+						spamDict[word] += 1.0
+
+	for fname in os.listdir(data_directory + '/notspam'):
+		with open(data_directory + '/notspam/' + fname, 'r') as f:
+			lines = f.readlines()
+		for line in lines:
+			for w in line.strip().split(' '):
+				word = w.lower()
+				if word not in ('', ' ', 'the', 'to'):
+					word = word.translate(None, string.punctuation)
+					if nonspamDict.get(word, None) == None:
+						nonspamDict[word] = 1.0
+					else:
+						nonspamDict[word] += 1.0
+
+	b1 = sorted(spamDict.items(), key=operator.itemgetter(1), reverse=True)[:len(spamDict) / dividend]
+	b2 = sorted(nonspamDict.items(), key=operator.itemgetter(1), reverse=True)[:len(nonspamDict) / dividend]
+
+	union_set = set(b1).union(set(b2))
+	attributes = [w[0] for w in union_set]
+	if '' in attributes:
+		attributes.remove('')
+
+	attributeSet = set(attributes)
+
+	dataTable = []
+
+	for fname in os.listdir(data_directory + '/spam'):
+		with open(data_directory + '/spam/' + fname, 'r') as f:
+			lines = f.readlines()
+		datarow = []
+		wordDict = {}
+		for line in lines:
+			for w in line.strip().split(' '):
+				word = w.lower()
+				if word not in ('', ' ', 'the', 'to'):
+					word = word.translate(None, string.punctuation)
+					if word in attributeSet:
+						if wordDict.get(word, None) == None:
+							wordDict[word] = 1
+						#else:
+						#	wordDict[word] += 1
+		for w in attributes:
+			datarow.append(wordDict.get(w, 0))
+		datarow.append(1)  # 1 for spam, 0 for not spam
+		dataTable.append(datarow)
+
+
+	for fname in os.listdir(data_directory + '/notspam'):
+		with open(data_directory + '/notspam/' + fname, 'r') as f:
+			lines = f.readlines()
+		datarow = []
+		wordDict = {}
+		for line in lines:
+			for w in line.strip().split(' '):
+				word = w.lower()
+				if word not in ('', ' ', 'the', 'to'):
+					word = word.translate(None, string.punctuation)
+					if word in attributeSet:
+						if wordDict.get(word, None) == None:
+							wordDict[word] = 1
+						#else:
+						#	wordDict[word] += 1
+
+		for w in attributes:
+			datarow.append(wordDict.get(w, 0))
+		datarow.append(0)  # 1 for spam, 0 for not spam
+		dataTable.append(datarow)
+
+	# each attributes is a word which is stored on the list called 'attributes'
+	# so, dataTable has 2646 rows and each row has 7417 attributes where the last attribute is target attribute
+	print len(dataTable)
+	print len(dataTable[0])
+	print attributes
+	# print attributeSet
+	print dataTable[0]
+	# build the decision tree
+	# write the decision tree model on the filename passed as 'model' so that later we can load the model and test new data instances
+	# creating the decision tree
+	# recursively create the decision tree
+	root = decisionTree(dataTable, attributes, root=None)
+	print root
+	# save the decision tree into the model file
+	with open(model, 'wb') as output:
+		pickle.dump(root, output, pickle.HIGHEST_PROTOCOL)
+	print 'Decision tree saved to memory'
+
+def testBinaryDecisionTree(data_directory, model):
+	nonspamDict = {}
+	spamDict = {}
+	print('Testing Binary Decision Tree Classifier... Please wait')
+	for fname in os.listdir(data_directory + '/spam'):
+		with open(data_directory + '/spam/' + fname, 'r') as f:
+			lines = f.readlines()
+			for line in lines:
+				for w in line.strip().split(' '):
+					word = w.lower()
+					# if word == '':
+					#	raw_input()
+					if word not in ('', ' ', 'the', 'to'):
+						word = word.translate(None, string.punctuation)
+						if spamDict.get(word, None) == None:
+							spamDict[word] = 1.0
+						else:
+							spamDict[word] += 1.0
+
+	for fname in os.listdir(data_directory + '/notspam'):
+		with open(data_directory + '/notspam/' + fname, 'r') as f:
+			lines = f.readlines()
+			for line in lines:
+				for w in line.strip().split(' '):
+					word = w.lower()
+					if word not in ('', ' ', 'the', 'to'):
+						word = word.translate(None, string.punctuation)
+						if nonspamDict.get(word, None) == None:
+							nonspamDict[word] = 1.0
+						else:
+							nonspamDict[word] += 1.0
+
+	b1 = sorted(spamDict.items(), key=operator.itemgetter(1), reverse=True)[:len(spamDict) / dividend]
+	b2 = sorted(nonspamDict.items(), key=operator.itemgetter(1), reverse=True)[:len(nonspamDict) / dividend]
+
+	union_set = set(b1).union(set(b2))
+	attributes = [w[0] for w in union_set]
+	if '' in attributes:
+		attributes.remove('')
+
+	attributeSet = set(attributes)
+
+	dataTable = []
+
+	for fname in os.listdir(data_directory + '/spam'):
+		with open(data_directory + '/spam/' + fname, 'r') as f:
+			lines = f.readlines()
+			datarow = []
+			wordDict = {}
+			for line in lines:
+				for w in line.strip().split(' '):
+					word = w.lower()
+					if word not in ('', ' ', 'the', 'to'):
+						word = word.translate(None, string.punctuation)
+						if word in attributeSet:
+							if wordDict.get(word, None) == None:
+								wordDict[word] = 1
+							else:
+								wordDict[word] += 1
+			for w in attributes:
+				datarow.append(wordDict.get(w, 0))
+			datarow.append(1)  # 1 for spam, 0 for not spam
+			dataTable.append(datarow)
+
+	for fname in os.listdir(data_directory + '/notspam'):
+		with open(data_directory + '/notspam/' + fname, 'r') as f:
+			lines = f.readlines()
+			datarow = []
+			wordDict = {}
+			for line in lines:
+				for w in line.strip().split(' '):
+					word = w.lower()
+					if word not in ('', ' ', 'the', 'to'):
+						word = word.translate(None, string.punctuation)
+						if word in attributeSet:
+							if wordDict.get(word, None) == None:
+								wordDict[word] = 1
+							#else:
+							#	wordDict[word] += 1
+
+			for w in attributes:
+				datarow.append(wordDict.get(w, 0))
+			datarow.append(0)  # 1 for spam, 0 for not spam
+			dataTable.append(datarow)
+
+	# each attributes is a word which is stored on the list called 'attributes'
+	# so, dataTable has 2646 rows and each row has 7417 attributes where the last attribute is target attribute
 	print len(dataTable)
 	print len(dataTable[0])
 	print attributes
@@ -319,18 +550,26 @@ def testDecisionTree(data_directory,model):
 	root = pickle.load(open(model, 'rb'))
 	print (root)
 	correct = 0
-	for i in range (0, len(dataTable)):
+	spam = 0
+	notspam = 0
+	for i in range(0, len(dataTable)):
 		dataTableRow = dataTable[i]
-		if dataTableRow[len(dataTableRow) -1] == 1:
+		if dataTableRow[len(dataTableRow) - 1] == 1:
 			actualResult = 'spam'
 		else:
 			actualResult = 'notspam'
 		predictedResult = traverseTree(root, dataTableRow, attributes)
+		if predictedResult == 'spam':
+			spam += 1
+		else:
+			notspam += 1
 		if actualResult == predictedResult:
 			correct += 1
 	print 'Accuracy'
 	print correct
-	print float(correct)/len(dataTable)
+	print float(correct) / len(dataTable)
+	print spam
+	print notspam
 
 mode,technique,data_directory,model = sys.argv[1:]
 if mode == 'train':
@@ -338,8 +577,10 @@ if mode == 'train':
 		trainNaiveBayes(data_directory,model)
 	else:
 		trainDecisionTree(data_directory,model)
+		#trainBinaryDecisionTree(data_directory, model)
 else:
 	if technique == 'bayes':
 		testNaiveBayes(data_directory,model)
 	else:
 		testDecisionTree(data_directory,model)
+		#testBinaryDecisionTree(data_directory, model)
